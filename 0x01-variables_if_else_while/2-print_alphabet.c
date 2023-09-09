720364a386d8d6fc2fd2e1a071ba3e8d57511adc
Infomohmed <kasskasemi@gmail.com>
2023-09-09 06:20:38 -0700

    helllo

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,15 +4,15 @@
  * main - Prints the alphabet.
  *
  * Return: Always 0 (success)
- */ 
+*/ 
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	for (i = 0;i < 26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
-        putchar('\n');
+putchar('\n');
 	return (0);
 }
